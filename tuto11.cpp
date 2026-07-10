18fc00b071552b29361ce8b4e015bff139431d2c
roshan dahal <rosud562@gmail.com>
2026-07-10 13:28:42 +0545

    Add tutorial11

---
+++ tuto11.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // what is pointer? ----> datatype which holds the address of other
+    // data types
+
+    int a = 3; 
+    // Syntax of pointer                             
+    int* b = &a;
+    // & ----> (Address of) Operator
+    cout<<"The address of a is "<<&a<<endl;
+    cout<<"The address of a is "<<b<<endl;
+    
+    
+    // * ---->(value at) Dereference operator
+    cout<<"The address of b is "<<*b<<endl;
+
+    //Pointer to Pointer
+    int ** c = &b;
+    cout<<"The address of b is "<<&b<<endl;
+    cout<<"The address of b is "<<c<<endl;
+    cout<<"The value at address c is "<<*c<<endl;
+    cout<<"The  value at address value_at (value_at (c)) is "<<**c<<endl;
+    
+
+    return 0;
+}
